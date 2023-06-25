071bd8a191579bcfa32562957f1f3cc21428cfd3
Leonardo Rodenas Escobar <leo.rodenas.e@gmail.com>
2023-06-25 14:48:43 -0400

    actualizo hasta tutorial min 1:37:30 parte casi final

--- ejercicio_individual/app_1/urls.py
+++ ejercicio_individual/app_1/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import ListaTareas, DetalleTarea, CrearTarea, ActualizarTarea, BorrarTarea
+from .views import ListaTareas, DetalleTarea, CrearTarea, ActualizarTarea, BorrarTarea, VistaLoginCustom
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
    path('', views.home, name='home'),
-   path('login/', views.IngresoView.as_view(), name='login'),
-   path('logout/', LogoutView.as_view(), name='logout'), 
+   path('login/', VistaLoginCustom.as_view(), name='login'),
+   path('logout/', LogoutView.as_view(next_page='login'), name='logout'), 
    path('lista_tareas/', ListaTareas.as_view(), name='lista_tareas'), 
    path('lista_tareas/<int:pk>/', DetalleTarea.as_view(), name='detalle_tarea'),
    path('crear_tarea/', CrearTarea.as_view(), name='crear_tarea'), 
--- ejercicio_individual/app_1/views.py
+++ ejercicio_individual/app_1/views.py
@@ -1,71 +1,75 @@
+from typing import Any
 from django.shortcuts import render
-from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate, login, logout
-from django.views.generic import TemplateView
-from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
-from .models import Tarea
+#Imports necesarios para el CRUD de tareas
+from .models import Tarea #Importo modelo a usar en las vistas del CRUD
 from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView 
 from django.urls import reverse_lazy
+#Imports necesarios para el login
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin #Mixim para restringuir vistas a usuarios que no esten logueados
+
 # Create your views here.
 
 #Renderiza Home 
 def home(request):
     return render(request, 'index.html')
 
-#Renderiza el Login
-class IngresoView(TemplateView):
+class VistaLoginCustom(LoginView):
     template_name = 'registration/login.html'
-
-    def get(self, request, *args, **kwargs):
-        form = LoginForm()
-        return render(request, self.template_name, {"form": form})
-
-    def post(self, request, *args, **kwargs):
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return redirect('lista_tareas')
-            form.add_error('username', 'Credenciales incorrectas')
-            return render(request, self.template_name, {"form": form})
-        else:
-            return render(request, self.template_name, {"form": form})
-
-#Renderiza Logout
-@login_required
-def logout_user(request):
-    logout(request)
-    return redirect('login')
+    fields = '__all__' # Crea todos los campos para el formulario a partir del modelo
+    redirect_authenticated_user = True # Rediderciona si el login es exitoso
+    
+    def get_success_url(self):
+        return reverse_lazy('lista_tareas') # Lugar al que se es redirecionado si el login es exitoso
 
 #Renderiza Tareas
-class ListaTareas(ListView):
+
+#Ver lista Tareas
+class ListaTareas(LoginRequiredMixin, ListView):
     model = Tarea # Modelo a utilizar
     context_object_name = 'Tareas' # Le da un nuevo nombre en el for para que no se llame simplemente object
     template_name = 'templates_app/app_1/lista_tareas.html' # modifica la ruta el template para que no sea necesario que se llame tarea_list.html (prefijo = modelo, sufijo=list, así lo busca por defecto al usar estas clases heredadas)
 
-class DetalleTarea(DetailView):
+    #Sobreescribo el método para recibir solo las tareas del usuario logueado y no las de todos los usuario en la db
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs) #genero un contexto
+        context ['Tareas'] = context ['Tareas'].filter(usuario = self.request.user) # A ese contexto le asigno las Tareas, pero filtradas de acuerdo al request.user que tengo en la plantilla de lista_tareas.html
+        context ['count'] = context ['Tareas'].filter(estado = True).count # Esto es para contar las tareas incompletas
+        #Entrada de valores y configuración de la búsqueda
+        input_busqueda = self.request.GET.get('search-area') or '' #Esto indica que la busqueda puede tener un valor o dejarse ne blanco ('')
+        if input_busqueda:
+            context ['Tareas'] = context ['Tareas'].filter(titulo__icontains = input_busqueda) # Acá busca por título
+        
+        context ['input_busqueda'] = input_busqueda      
+        return context
+
+#Ver Detalle Tareas
+class DetalleTarea(LoginRequiredMixin, DetailView):
     model = Tarea # Modelo a utilizar
     context_object_name = 'Tarea' # Le da un nuevo nombre en el for para que no se llame simplemente object
     template_name = 'templates_app/app_1/detalle_tarea.html' # modifica la ruta el template para que no sea necesario que se llame tarea_detail.html (prefijo = modelo, sufijo=detail, así lo busca por defecto al usar estas clases heredadas)
 
-class CrearTarea(CreateView):
-     model = Tarea # Modelo a utilizar
-     fields = '__all__' # Crea todos los campos para el formulario a partir del modelo
-     success_url = reverse_lazy('lista_tareas') # Al tener exito al crear la tarea redirigir a lista_tareas
+#Crear Tareas
+class CrearTarea(LoginRequiredMixin, CreateView):
+    model = Tarea # Modelo a utilizar
+    fields = ['titulo', 'descripcion', 'estado'] # Crea los campos para el formulario a partir del modelo
+    success_url = reverse_lazy('lista_tareas') # Al tener exito al crear la tarea redirigir a lista_tareas
+     
+     #Sobreescribo este método para hacer que las tareas creadas sean agregadas al usuario que está logueado y no al primer usuario en la db
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super(CrearTarea, self).form_valid(form)
 
-class ActualizarTarea(UpdateView):
+#Actualizar Tareas
+class ActualizarTarea(LoginRequiredMixin, UpdateView):
      model = Tarea # Modelo a utilizar
-     fields = '__all__' # Crea todos los campos para el formulario a partir del modelo
+     fields = ['titulo', 'descripcion', 'estado'] # Crea todos los campos para el formulario a partir del modelo
      success_url = reverse_lazy('lista_tareas') # Al tener exito al actualizar la tarea redirigir a lista_tareas
 
-class BorrarTarea(DeleteView):
+#Borrar Tareas
+class BorrarTarea(LoginRequiredMixin, DeleteView):
     model = Tarea # Modelo a utilizar
     context_object_name = 'Tareas' # Le da un nuevo nombre en el for para que no se llame simplemente object
     success_url = reverse_lazy('lista_tareas') # Al tener exito al borrar la tarea redirigir a lista_tareas
